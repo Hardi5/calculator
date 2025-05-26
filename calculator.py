20f4b30146221f133d416eec90998b5f3a552bbf
hardi5 <hardi.kakalaw@chasacademy.se>
2025-05-26 17:32:08 +0200

    some changes to calc

--- calculator.py
+++ calculator.py
@@ -1,5 +1,22 @@
-print("Enter the first numer:")
-num1 = int(input())
-print("Enter the second number:")
-num2 = int(input())
-print("enter the operation:")
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide (n1, n2):
+    return n1 / n2
+
+print("Welcome to the calculator program!")
+print("Please select an operation -\n"
+      "1. Add\n"
+      "2. Subtract\n"
+      "3. Multiply\n"
+      "4. Divide\n")
+
+selection = int(input("Select 1-4 from the menu: "))
+
+# https://www.geeksforgeeks.org/make-simple-calculator-using-python/
